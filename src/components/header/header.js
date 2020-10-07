b25ce557764be2c6bb3f2d7ae677a191da5572f8
ThaysMrtns <thays.martins@nave.org.br>
2020-10-07 14:31:01 -0300

    Adicionando imagem

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,20 @@
 //Importar o React
 import React from 'react';
+//Incluir os arquivos css
+import style from '../header/header.module.css';
+//Importar a imagem
+import img from '../img/img.jpg';
 
 
 //Criar a classe com o nome Headers
 class Header extends React.Component{
     render(){
         return(
-            <h2 className="header">Header</h2>
+            <div>
+                <h2 className={style.titulo}>Header</h2>
+                <img src={img}/>
+            </div>
+            
         );
     }
 }
