f22f37bd4d76fc43fc408e2002fa61c7c1b14acc
ThaysMrtns <thays.martins@nave.org.br>
2020-10-28 12:44:42 -0200

    Lidando com eventos e bind

--- src/components/content/form.js
+++ src/components/content/form.js
@@ -1,16 +1,28 @@
 //Importar o React
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 //Criar a classe com o nome Form
 class Form extends React.Component{
+    constructor(){
+        //Pq utilizar o super? Para referênciar a classe pai?
+        //Estou utilizando método de uma instância, this
+        super();
+        this.email ="";
+    }
+
+    handleEvento(evento){
+        this.email = console.log(evento.target.value);
+    }
     render(){
         return(
             <form>
                 <label>Email de usuário: </label>
-                <input type="text"></input><br></br>
+                <input onChange={this.handleEvento.bind(this)} type="text"></input><br></br>
                 <label>Senha: </label>
                 <input type="text"></input><br></br>
-                <button type="submit">Entrar</button>
+                <button type="submit">Enviar</button>
+                
             </form>
         );
     }
